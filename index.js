a5d2cc4ff6b67fc264c1820d7e24f85b48673494
Abhimanyu <43077595+a12baghel2@users.noreply.github.com>
2021-02-12 19:39:20 +0530

    added views and database connection

--- index.js
+++ index.js
@@ -2,23 +2,37 @@
 const  express = require('express');
 const mongoose = require('mongoose');
 const Article = require('./models/articles');
+require('dotenv/config');
 const articleRouter = require('./routes/articles');
+const methodOverride = require('method-override');
 
 // Initialise express
 const app = express();
 
 //Connect to the database
-// connection code goes here
+mongoose.connect(process.env.DB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}, () => {
+    console.log("Connected to the database.....");
+});
 
 // set template view engine
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+
+// Method overriding
+app.use(methodOverride("_method"));
 
 //Routes middleware
 app.use('/articles', articleRouter);
 
 //Routes
-app.get('/', (req,res) => {
-    res.render('articles/index');
+app.get('/', async (req,res) => {
+    const articles = await Article.find().sort({createdAt: 'desc'})
+    res.render('articles/index', { articles: articles });
 });
 
 app.get('/:name', (req,res) => {
